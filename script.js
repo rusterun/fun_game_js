3272c42ea301e159a47addc3b9bb742335283052
Ivan Sarapultsev <62208960+rusterun@users.noreply.github.com>
2021-04-09 10:59:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ var hard=false;
 var easy=false;
 
 window.onload = function() {
+	var diff = document.getElementById('difficult');
+	var el2 = document.getElementById("linka2");
+	var element = document.getElementById("linka");
 	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 	// код для мобильных устройств
 		alert('Извините, но игра не поддерживается на мобильных устройствах');
+		el2.style.width = screen.width;
+		el2.style.height = screen.height;
+		diff.innerHTML = "";
+		element.innerHTML = "<h1>Sorry</h1>";
 	} else {
-		var diff = document.getElementById('difficult');
-		var el2 = document.getElementById("linka2");
-		var element = document.getElementById("linka");
 		element.style.top = "0px";
 		//el.style.top = "276px";
 		element.onmouseover=function() {

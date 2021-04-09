5288c5d0d41cee60247313b4d3e3f591f342d361
Ivan Sarapultsev <62208960+rusterun@users.noreply.github.com>
2021-04-09 10:28:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ var hard=false;
 var easy=false;
 
 window.onload = function() {
-	var diff = document.getElementById('difficult');
-	var el2 = document.getElementById("linka2");
-	var element = document.getElementById("linka");
-	element.style.top = "0px";
-	//el.style.top = "276px";
-	element.onmouseover=function() {
-	if (!easy) {
-		if (element.style.top == "0px") {element.style.top = "275px";}
-		else {element.style.top = "0px";}
-	}
-	let rand = Math.floor(Math.random()*185);
-	element.style.left = `${rand}px`;
-	};
-    
-	
-	el2.onmouseover=function() {
-		if (hard) {
-		element.style.color = "gray";
-	}
-	};
-	
-	el2.onmouseout=function() {
-		if (hard) {
-		element.style.color = "blue";
-	};
-	
-	};
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+	// код для мобильных устройств
+		alert('Извините, но игра не поддерживается на мобильных устройствах');
+	} else {
+		var diff = document.getElementById('difficult');
+		var el2 = document.getElementById("linka2");
+		var element = document.getElementById("linka");
+		element.style.top = "0px";
+		//el.style.top = "276px";
+		element.onmouseover=function() {
+		if (!easy) {
+			if (element.style.top == "0px") {element.style.top = "275px";}
+			else {element.style.top = "0px";}
+		}
+		let rand = Math.floor(Math.random()*185);
+		element.style.left = `${rand}px`;
+		};
 
+
+		el2.onmouseover=function() {
+			if (hard) {
+			element.style.color = "gray";
+		}
+		};
+
+		el2.onmouseout=function() {
+			if (hard) {
+			element.style.color = "blue";
+		};
+
+		};
+	};
 console.log("Info: "+(element.style.top=="0px"));
 
 };
